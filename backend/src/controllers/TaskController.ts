a29129f49ca0da2472cb27185ef7873f438d2b68
Alejandro Osvaldo Cinelli Coria <aocinellicoria@teco.com.ar>
2024-12-07 01:16:51 -0300

    V3.1

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -1,14 +1,27 @@
 import type {Request, Response} from 'express'
+import Project from '../models/Proyect'
+import Task from '../models/Task'
 
 export class TaskController {
 
     static createTask = async (req : Request, res : Response) => {
         
         const { projectId } = req.params
-        console.log(projectId) 
+        
+        const project = await Project.findById(projectId)
+        if(!project){
+            const error = new Error("Proyecto No Encontrado")
+            res.status(404).json({error : error.message })
+        }
+       
         try {
-           
-            res.send("Project creado OK")
+            const task = new Task(req.body)
+            task.project = project.id
+            project.tasks.push(task)
+            
+            await task.save()
+            await project.save()
+            res.send(project)
         } catch (error) {
             console.log(error)
         }
